7946a78a8a91c53355f228b0c8eb96bf84ab6a02
Xayse6 <xays3mpena@gmail.com>
2026-03-17 08:44:59 -0300

    0

--- localshop-main/src/@types/loja.ts
+++
@@ -1,8 +0,0 @@
-export interface Loja{
-    id:string;
-    nome: string;
-    categoria: string;
-    image: string;
-    distancia:string;
-    descricao?: string; // 0 '?' indica campo opcional
-}
--- tesste/src/models/Map.ts
+++
@@ -1,7 +0,0 @@
-export interface GameMap{
-
-name:string
-region:string
-description:string
-
-}
