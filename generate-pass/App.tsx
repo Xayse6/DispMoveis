43854139d8200935e685e7766c23b03f3a315585
Xayse6 <xays3mpena@gmail.com>
2026-04-29 11:45:42 -0300

    8

--- generate-pass/App.tsx
+++ generate-pass/App.tsx
@@ -1,12 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native';
-import{Home} from "./src/screens/Home/Home"
+import React from "react";
+import{View,Text, Image} from "react-native";
 
-export default function App() {
-  return (
-    <View>
-      <Home/>
-    </View>
-  );
+import {Logo} from "../../assets/image/olho.png";
+import {styles} from "./LogoStyles";
+
+export function LogoComponent(){
+  console.log("Entri no App");  
+  return(
+    <>
+    <Text style={styles.title}>Pass Generator</Text>
+    <Image source={Logo} style={{resizeMode:"contain", height:180}}/>
+    </>
+    );
 }
-
-
--- generate-pass/src/components/Logo.tsx
+++ generate-pass/src/components/Logo.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import{View,Text, Image} from "react-native";
+import { Text, Image } from "react-native";
 
-import {Logo} from "./olho.png";
-import {styles} from "./LogoStyles";
+import Logo from "../../assets/image/olho.png";
+import { styles } from "./LogoStyles";
 
-export function LogoComponent(){
-    return(
+export function LogoComponent() {
+  return (
     <>
-    <Text style={styles.title}>Pass Generator</Text>
-    <Image source={Logo} style={{resizeMode:"contain", height:180}}/>
+      <Text style={styles.title}>Pass Generator</Text>
+      <Image source={Logo} style={{ resizeMode: "contain", height: 180 }} />
     </>
-    );
+  );
 }
